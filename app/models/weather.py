fcd8d1f537da7f8fa2d29019e7d1e48d6a77f978
Ashika <ashikashrestha114@gmail.com>
2026-02-25 14:10:07 +0545

    bugfix: added data to db correctly and added new variables

--- app/models/weather.py
+++ app/models/weather.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy import Column, Integer, String, Float, DateTime, func
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -8,7 +8,7 @@ class WeatherRecord(Base):
 
     id                = Column(Integer, primary_key=True, autoincrement=True)
     city              = Column(String(100), nullable=False)
-    scraped_at        = Column(DateTime, nullable=False)
+    scraped_at        = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     temperature       = Column(Float)
     real_feel         = Column(Float)
     weather_condition = Column(String(255))
@@ -17,3 +17,8 @@ class WeatherRecord(Base):
     wind_direction    = Column(String(10))
     visibility        = Column(Float)
     max_uv_index      = Column(Float)
+    dew_point         = Column(Float)
+    pressure          = Column(Float)
+    cloud_cover       = Column(Float)
+    cloud_ceiling     = Column(Float)
+    task_id           = Column(String(36))
