e491a55dc26e16296fca1b270162f90b82a9a7c0
Ashika <ashikashrestha114@gmail.com>
2026-08-13 10:12:50 +0545

    #feat/WS-12 convert db from MySQL to PostgreSQL

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,31 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 from flask_migrate import Migrate
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
+
+
 def create_app():
     app = Flask(__name__)
+
+    postgres_user = os.environ["POSTGRES_USER"]
+    postgres_password = os.environ["POSTGRES_PASSWORD"]
+    postgres_host = os.environ["POSTGRES_HOST"]
+    postgres_port = os.getenv("POSTGRES_PORT", "5432")
+    postgres_db = os.environ["POSTGRES_DB"]
+
     app.config["SQLALCHEMY_DATABASE_URI"] = (
-        f"mysql+pymysql://{os.getenv('MYSQL_USER', 'user')}:{os.getenv('MYSQL_PASSWORD', 'password')}@{os.getenv('MYSQL_HOST', 'db')}/{os.getenv('MYSQL_DB', 'weather_db')}"
+        f"postgresql+psycopg://"
+        f"{postgres_user}:{postgres_password}@"
+        f"{postgres_host}:{postgres_port}/"
+        f"{postgres_db}"
     )
+
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
     db.init_app(app)
-    migrate.init_app(app, db) 
+    migrate.init_app(app, db)
+
     return app
